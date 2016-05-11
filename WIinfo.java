312da928985cc5b7cfc7b2c13009a83f50384356
M4l2tllV <M4l2tIlV@users.noreply.github.com>
2016-05-12 00:40:35 +0700

    Add more file
    For OOSD Concept

---
+++ WIinfo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package termproject_wh;
+
+/**
+ *
+ * @author KLoOonal
+ */
+public interface WIinfo {
+    String[][] getInfo(int typ);
+    String[][] genInfo(String sql);
+}
