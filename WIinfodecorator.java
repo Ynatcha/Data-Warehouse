604841972f296b0d4c4f348ce8a9b6e8b6e41af0
M4l2tllV <M4l2tIlV@users.noreply.github.com>
2016-05-13 04:36:49 +0700

    Add More info file
    For Info file testing.

---
+++ WIinfodecorator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package termproject_wh;
+
+/**
+ *
+ * @author KLoOonal
+ */
+public abstract class WIinfodecorator implements WIinfo{
+    protected WIinfo decoratedInfo;
+    
+    public WIinfodecorator(WIinfo dcorated){
+        this.decoratedInfo = dcorated;
+    }
+    
+    public String[][] getInfo(int typ){
+       
+    return decoratedInfo.getInfo(typ);
+    }
+    
+    public String[][] genInfo(String sql){
+        
+     return decoratedInfo.genInfo(sql);
+    }
+}
